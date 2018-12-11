d64adb890f1db48a64e6e2ef118b9a64d8d4a5b3
Christopher Amanor <kriskit7@gmail.com>
2018-12-11 11:01:34 +0000

    fixed redirect issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   
-  devise_scope :user do
-    root 'devise/sessions#new'
-  end
+  # devise_scope :user do
+  #   root 'devise/sessions#new'
+  # end
 
 
   resources :documents
   resources :applications
   # root 'devise/sessions#new'
-  #root 'applications#index'
+  root 'applications#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
