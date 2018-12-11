ecf28af4844c9cc0027e6f5c674d5dcf4a3f8f5e
Christopher Amanor <kriskit7@gmail.com>
2018-12-11 12:57:57 +0000

    started added interface to application pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  
   # devise_scope :user do
   #   root 'devise/sessions#new'
   # end
-
-
   resources :documents
   resources :applications
-  # root 'devise/sessions#new'
   root 'applications#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
