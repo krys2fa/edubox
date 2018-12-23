7832bded6e39fbaaaedf188218eb3203d0c400b2
Christopher Amanor <kriskit7@gmail.com>
2018-12-23 20:26:37 +0000

    worked on menus and started work on dashboard page

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class DashboardController < ApplicationsController
+    before_action :authenticate_user!
+
+    def index
+
+        
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :documents
   resources :applications
   root 'applications#index'
+  get 'dashboard/index' => 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
