f3448b4cd46ba53be211d3393be95de398e8c6e4
Christopher Amanor <kriskit7@gmail.com>
2018-12-30 18:28:24 +0000

    working on fixing showing of school in view

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,11 @@
 class Application < ApplicationRecord
   has_one :document
   belongs_to :user
-  has_one :institution
+  belongs_to :institution
   validates :firstname, :lastname, :dob, :enrolled, :completed, 
   			:quantity, :studentid, :department, :college, :phone, :institution_id,
         :programme, :address, :reason, :presence => true
+
+  
         
 end
